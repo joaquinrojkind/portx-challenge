271fb57906d2a25ccb64a951ba86bdd64c019483
Joaquin Rojkind <joaquin.rojkind@Joaquins-MBP.fibertel.com.ar>
2023-03-18 19:09:52 -0300

    Refactoring

--- src/main/java/com/portx/payment/service/PaymentServiceImpl.java
+++ src/main/java/com/portx/payment/service/PaymentServiceImpl.java
@@ -41,7 +41,7 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public com.portx.payment.service.model.Status checkPaymentStatus(Long paymentId) {
         return com.portx.payment.service.model.Status.valueOf(
-                paymentRepository.findById(paymentId).orElseThrow(() -> new EntityNotFoundException())
+                paymentRepository.findById(paymentId).orElseThrow(EntityNotFoundException::new)
                         .getStatus().name());
     }
 
@@ -53,21 +53,21 @@ public class PaymentServiceImpl implements PaymentService {
         AccountEntity receiver;
 
         if (Objects.nonNull(payment.getOriginator().getId())) {
-            originator = userRepository.getOne(payment.getOriginator().getId());
+            originator = userRepository.findById(payment.getOriginator().getId()).orElseThrow(EntityNotFoundException::new);
         } else {
             originator = UserEntity.builder()
                     .name(payment.getOriginator().getName())
                     .build();
         }
         if (Objects.nonNull(payment.getBeneficiary().getId())) {
-            beneficiary = userRepository.getOne(payment.getBeneficiary().getId());
+            beneficiary = userRepository.findById(payment.getBeneficiary().getId()).orElseThrow(EntityNotFoundException::new);
         } else {
             beneficiary = UserEntity.builder()
                     .name(payment.getBeneficiary().getName())
                     .build();
         }
         if (Objects.nonNull(payment.getSender().getId())) {
-            sender = accountRepository.getOne(payment.getSender().getId());
+            sender = accountRepository.findById(payment.getSender().getId()).orElseThrow(EntityNotFoundException::new);
         } else {
             sender = AccountEntity.builder()
                     .accountType(payment.getSender().getAccountType())
@@ -75,14 +75,13 @@ public class PaymentServiceImpl implements PaymentService {
                     .build();
         }
         if (Objects.nonNull(payment.getReceiver().getId())) {
-            receiver = accountRepository.getOne(payment.getReceiver().getId());
+            receiver = accountRepository.findById(payment.getReceiver().getId()).orElseThrow(EntityNotFoundException::new);
         } else {
             receiver = AccountEntity.builder()
                     .accountType(payment.getReceiver().getAccountType())
                     .accountNumber(payment.getReceiver().getAccountNumber())
                     .build();
         }
-
         return PaymentEntity.builder()
                 .currency(payment.getCurrency())
                 .amount(payment.getAmount())
